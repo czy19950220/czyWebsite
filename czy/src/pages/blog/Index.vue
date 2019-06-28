1e06a996b77fa860e74567310c3678eb314bdf16
2772553223@qq.com <zhenyu19950220>
2019-06-28 08:53:27 +0800

    优化页面加载问题，把froala编辑器的文件引入到组件页面中

--- czy/src/pages/blog/Index.vue
+++ czy/src/pages/blog/Index.vue
@@ -45,7 +45,13 @@
 </template>
 
 <script>
-
+  // Require Froala Editor js file.
+  require('froala-editor/js/froala_editor.pkgd.min.js')
+  // Require Froala Editor css files.
+  require('froala-editor/css/froala_editor.pkgd.min.css')
+  require('font-awesome/css/font-awesome.css')
+  require('froala-editor/css/froala_style.min.css')
+  require ('froala-editor/js/languages/zh_cn')
   export default {
     components: {},
     name: "Index",
