bd523a7573e555c84e0dd19d2e5dd3af81e7b121
2772553223@qq.com <zhenyu19950220>
2019-07-18 16:53:01 +0800

    有待重构

--- czy/src/pages/index/Index.vue
+++ czy/src/pages/index/Index.vue
@@ -16,18 +16,8 @@
     },
     methods: {},
     created() {
-      this.$axios.post(`${this.$sIP2}/novel/search`, this.ruleForm).then((res) => {
-        //console.log(res.data.result)
-        let reader = new FileReader();
-        let str1 = onload.readAsText(res.data.result,["utf-8"]);
-        let str2 = onload.readAsText(res.data.result,["GBK"]);
-        console.log(str1)
-        console.log(str2)
-        /*let reader = new FileReader();
-        reader.readAsText(data, "GBK");
-        reader.onload = function (e) {
-          console.log(reader.result);
-        };*/
+      this.$axios.get(`${this.$sIP2}/novel/search`).then((res) => {
+        console.log(res.data)
       }).catch(err => {
         console.log(err)
       })
