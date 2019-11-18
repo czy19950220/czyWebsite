ee2fd3fe9efbfb094a4cbc9e5abbf74fde885a08
2772553223@qq.com <zhenyu19950220>
2019-11-18 09:52:43 +0800

    1

--- czy/src/App.vue
+++ czy/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <router-view/>
     <canvas id="c" style="position: absolute;z-index: -1;text-align: center;z-index: -1;top: 0px;left: 0px;"></canvas>
-    <span style="color: #999;position: fixed;bottom: 20px;left: calc(50% - 140px);">ICP主体备案号:豫ICP备19022986号-1<i class="icon-beian"></i></span>
+    <a href="www.beian.miit.gov.cn" style="color: #999;position: fixed;bottom: 20px;left: calc(50% - 140px);">ICP主体备案号:豫ICP备19022986号-1<i class="icon-beian"></i></a>
   </div>
 </template>
 
